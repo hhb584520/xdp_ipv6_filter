c17d617e653c9b1844e63cb6d124cce9985ff884
Haibin Huang <haibin.huang@intel.com>
2022-08-01 11:15:03 +0800

    add iphdr
    Signed-off-by: Haibin Huang <haibin.huang@intel.com>

--- xdp_ipv6_filter.c
+++ xdp_ipv6_filter.c
@@ -34,9 +34,32 @@ bool parse_eth(struct ethhdr *eth, void *data_end, u16 *eth_type)
     offset = sizeof(*eth);
     if ((void *)eth + offset > data_end)
         return false;
-	*eth_type = eth->h_proto;
-	return true;
+    
+    *eth_type = eth->h_proto;
+    
+    bpf_debug("Debug: h_source:%x:%x\n", eth->h_source[0], eth->h_source[5]);
+    bpf_debug("Debug: h_dest:%x:%x\n", eth->h_dest[0], eth->h_dest[5]);
+
+    return true;
 }
+
+static __always_inline
+bool parse_ip(struct iphdr *ip, void *data_end, u8 *ip_type)
+{
+    u64 offset;
+
+    offset = sizeof(*ip);
+    if ((void *)ip + offset > data_end)
+        return false;
+    
+    *ip_type = ip->protocol;
+    
+    bpf_debug("Debug: source ip:0x%x\n", ip->saddr);
+    bpf_debug("Debug: dest ip:0x%x\n", ip->daddr);
+
+    return true;
+}
+
 
 SEC("prog")
 int xdp_ipv6_filter_program(struct xdp_md *ctx)
@@ -44,19 +67,27 @@ int xdp_ipv6_filter_program(struct xdp_md *ctx)
     void *data_end = (void *)(long)ctx->data_end;
     void *data     = (void *)(long)ctx->data;
     struct ethhdr *eth = data;
-	u16 eth_type = 0;
+    struct iphdr *ip = data + sizeof(*eth);
+    u16 eth_type = 0;
+    u8 ip_type = 0;
 
-	if (!(parse_eth(eth, data_end, &eth_type))) {
+    if (!(parse_eth(eth, data_end, &eth_type))) {
         bpf_debug("Debug: Cannot parse L2\n");
         return XDP_PASS;
     }
 
+    if (!(parse_ip(ip, data_end, &ip_type))) {
+        bpf_debug("Debug: Cannot parse L3\n");
+        return XDP_PASS;
+    }
+
     bpf_debug("Debug: eth_type:0x%x\n", ntohs(eth_type));
-	if (eth_type == ntohs(0x86dd)) {
-		return XDP_PASS;
-	} else {
-		return XDP_DROP;
-	}
+    bpf_debug("Debug: ip_type:%u\n", ip_type);
+    if (eth_type == ntohs(0x86dd)) {
+        return XDP_PASS;
+    } else {
+        return XDP_DROP;
+    }
 }
 
 char _license[] SEC("license") = "GPL";
